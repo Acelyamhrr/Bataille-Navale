3c8ef02033b412721c39af095a643373a68d2559
Elora Fouilleul <efouilleul@unistra.fr>
2025-11-18 11:08:35 +0100

    Package model.game : implémentation. Ajout getteurs ResultAttack UML

--- Bataille Navale/src/model/game/Action.java
+++ Bataille Navale/src/model/game/Action.java
@@ -1,4 +1,20 @@
 package model.game;
 
+import model.contents.weapons.Weapon;
+import model.players.Player;
+
 public class Action {
+    private int round;
+    private int[] position;
+    private Player player;
+    private Weapon weapon;
+    private ResultAttack result;
+
+    public Action(int round, int x, int y, Player player, Weapon weapon, ResultAttack result) {
+        this.round = round;
+        this.position = new int[]{x, y};
+        this.player = player;
+        this.weapon = weapon;
+        this.result = result;
+    }
 }
--- Bataille Navale/src/model/game/History.java
+++ Bataille Navale/src/model/game/History.java
@@ -1,4 +1,23 @@
 package model.game;
 
+import java.util.ArrayList;
+
 public class History {
+    private ArrayList<Action> actions;
+
+    public History() {
+        this.actions = new ArrayList<Action>();
+    }
+
+    public void addAction(Action action){
+        this.actions.add(action);
+    }
+
+    public Action getLast(){
+        return this.actions.getLast();
+    }
+
+    public void reset(){
+        this.actions.clear();
+    }
 }
