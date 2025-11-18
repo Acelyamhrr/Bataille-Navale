1f73eced66e786cd734dee9044e2d7253b2b7844
Elora Fouilleul <efouilleul@unistra.fr>
2025-11-18 11:46:40 +0100

    Package model.weapons : mise en place des méthodes vides + factory fonctionnelle

--- Bataille Navale/src/model/contents/weapons/Weapon.java
+++ Bataille Navale/src/model/contents/weapons/Weapon.java
@@ -1,4 +1,26 @@
 package model.contents.weapons;
 
-public class Weapon {
+import model.contents.Content;
+import model.enums.WeaponType;
+import model.game.ResultAttack;
+import model.grid.Grid;
+
+public abstract class Weapon implements Content {
+    private int[] position;
+    private WeaponType name;
+
+    public Weapon(WeaponType name){
+        this.name = name;
+    }
+
+    public abstract ResultAttack use(Grid grid, int x, int y);
+
+    public void setPosition(int x, int y){
+        position = new int[]{x,y};
+    }
+
+    @Override
+    public int[] getPosition() {
+        return this.position;
+    }
 }
