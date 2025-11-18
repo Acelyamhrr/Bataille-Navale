d6aab91d28b1a759cb830d7162d8edf068e45e26
Elora Fouilleul <efouilleul@unistra.fr>
2025-11-18 10:56:18 +0100

    Package model.players : début implémentation -> mise en place méthodes vides et attributs. Suppresion dans UML méthodes non abstraites de Player dans classes filles

--- Bataille Navale/src/model/players/HumanPlayer.java
+++ Bataille Navale/src/model/players/HumanPlayer.java
@@ -1,4 +1,12 @@
 package model.players;
 
-public class HumanPlayer {
+import model.contents.weapons.Weapon;
+
+public class HumanPlayer extends Player{
+    public HumanPlayer(String name) {
+        super(name, false);
+    }
+
+    @Override
+    public void play(Weapon weapon, int x, int y) {}
 }
--- Bataille Navale/src/model/players/RobotPlayer.java
+++ Bataille Navale/src/model/players/RobotPlayer.java
@@ -1,4 +1,12 @@
 package model.players;
 
-public class RobotPlayer {
+import model.contents.weapons.Weapon;
+
+public class RobotPlayer extends Player{
+    public RobotPlayer(String name) {
+        super(name, true);
+    }
+
+    @Override
+    public void play(Weapon weapon, int x, int y) {}
 }
